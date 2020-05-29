808de872c62d8327c0b3325940968bd8cd2e2799
jessegersensonchess <59052632+jessegersensonchess@users.noreply.github.com>
2020-05-29 10:06:12 +0200

    Create lc0gpu.sh

---
+++ lc0gpu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#exec /root/lc0/./lc0 --minibatch-size=4 --max-prefetch=0 --threads=45 --backend-opts=blas_cores=1 --syzygy-paths=/data/tmptablebases
+exec /root/lc0/./lc0 --show-hidden
